47f3309f5b82239a432ffc5db31eadd7b41fa9d3
ronin010 <user_rh99@protonmail.com>
2021-07-06 14:17:43 +0100

    test

--- server.js
+++ server.js
@@ -25,4 +25,4 @@ if (process.env.NODE_ENV === "production") {
 
 const port = process.env.PORT || 8000
 
-app.listen(8000, () => console.log("Server started on port 8000"));
+app.listen(port, () => console.log("Server started on port" + port));
